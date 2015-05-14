71fb67732c1997d60f9461e5e2e73caac60c5e3b
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-13 23:38:09 -0500

    Added file renaming portion of program. Need to fix string.translate function issue

--- Bulk Name Change/rename_files.py
+++ Bulk Name Change/rename_files.py
@@ -8,16 +8,19 @@ class RenameFiles(Tkinter.Frame):
         # options for buttons
         button_opt = {'fill': Tkconstants.BOTH, 'padx': 1, 'pady': 1}
         Tkinter.Button(self, text='Rename Files', command=self.rename_files).pack(**button_opt)
-    
-    
-      
 
     def rename_files(self):
     #(1) get file names from a folder
         dirname = tkFileDialog.askdirectory()
         file_list = os.listdir(dirname)
-        print(file_list)
-        return
+        saved_path = os.getcwd()
+        print("Current Working Directory is " + saved_path)
+        os.chdir(dirname)
+    #(2) for each file, rename filename
+        for file_name in file_list:
+            os.rename(file_name, file_name.translate(None, "0123456789"))
+        os.chdir(saved_path)
+    
 
 if __name__=='__main__':
   root = Tkinter.Tk()
