18715f1c09592440fe455f0db91c2992822ab789
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-17 18:16:25 -0500

    Adding all files

--- SendTextMessage/send_text.py
+++ SendTextMessage/send_text.py
@@ -6,6 +6,6 @@ auth_token  = "34ac6f9f4e3333bd878cd7f783e1293b"
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="My name is Elliott Foster!",
-    to="+18328005842",    # Replace with your phone number
+    to="+12817364881",    # Replace with your phone number
     from_="+12816773601") # Replace with your Twilio number
 print message.sid
--- checkForProfan/profanityCheck.py
+++ checkForProfan/profanityCheck.py
@@ -4,21 +4,26 @@ import urllib
 
 
 def select_file():
-	Tk().withdraw() #pervents full TK GUI from displaying 
-	filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-	return(filename)
+        Tk().withdraw() #pervents full TK GUI from displaying 
+        filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+        return(filename)
 
 def read_text():
-	quotes = open(select_file())
-	contents_of_file = quotes.read()
-	print(contents_of_file)
-	quotes.close()
-	check_profanity(contents_of_file)
+        quotes = open(select_file())
+        contents_of_file = quotes.read()
+        print(contents_of_file)
+        quotes.close()
+        check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-	connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
-	output = connection.read()
-	print(output)
-	connection.close()
-	
+        connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+        output = connection.read()
+        connection.close()
+        if "true" in output:
+            print("Profanity Alert!!!!!")
+        elif "false" in output:
+            print("This document has no curse words!")
+        else:
+            print("Could not scan the document properly")
+        
 read_text()
