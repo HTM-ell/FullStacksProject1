84b5ec690d846621e29be35a71149a87299d0698
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-17 23:28:38 -0500

    last commit of the nite

--- MovieWebSite/media.py
+++ MovieWebSite/media.py
@@ -1,6 +1,8 @@
 import webbrowser
+import imdb
 
 class Movie():
+	
 	def __init__(self, movie_title,movie_storyline,poster_image,trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline, 
