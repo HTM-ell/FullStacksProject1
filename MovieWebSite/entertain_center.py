b8b25f0850f4689bf8a29a265c9cd48b915c75db
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-17 21:18:23 -0500

    completed modifications to entertain_center.py imported fresh_tomatoes. Generated movie trailer website

--- MovieWebSite/entertain_center.py
+++ MovieWebSite/entertain_center.py
@@ -1,13 +1,27 @@
 import media
+from fresh_tomatoes import open_movies_page as omp
 
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
-						"http://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+						"http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v7_aa.jpg",
 						"www.youtube.com/watch?v=KYz2wyBy3kc")
 						
 avatar = media.Movie("Avatar",
 					 "A marine on an alien planet",
-					 "http://en.wikipedia.org/wiki/Avatar_%282009_film%29#/media/File:Avatar-Teaser-Poster.jpg",
+					 "http://khabinh.vn/images/files/avatar-oridginal.jpg",
 					 "www.youtube.com/watch?v=cRdxXPV9GNQ")
-toy_story.show_trailer()
-					 
+
+sweeney_Todd = media.Movie("Sweeney Todd: The Demon Barber of Fleet Street",
+						   "story of Benjamin Barker, alias Sweeney Todd, who returns to London after 15 years'" +
+						   "transportation on trumped-up charges, to take revenge on the judge who banished him",
+						   "http://ia.media-imdb.com/images/M/MV5BMTg3NjUxMzM5NV5BMl5BanBnXkFtZTcwMzQ1NjQzMw@@._V1_SX640_SY720_.jpg",
+						   "www.youtube.com/watch?v=_4R72KROZ20")
+						   
+pacific_Rim = media.Movie("Pacific Rim",
+						  "a former pilot and a trainee are paired up to drive a seemingly obsolete special weapon",
+						  "http://vignette2.wikia.nocookie.net/pacificrim/images/2/27/Postery06u.jpg/revision/latest?cb=20130728052931",
+						  "www.youtube.com/watch?v=5guMumPFBag")
+						  
+movies = [toy_story, avatar, sweeney_Todd, pacific_Rim]
+
+omp(movies)
