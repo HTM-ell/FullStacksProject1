3104ef792c003a662f55046431e30e938929c8ab
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-17 23:35:52 -0500

    Revert "last commit of the nite"
    This reverts commit 84b5ec690d846621e29be35a71149a87299d0698.

--- MovieWebSite/media.py
+++ MovieWebSite/media.py
@@ -1,8 +1,6 @@
 import webbrowser
-import imdb
 
 class Movie():
-	
 	def __init__(self, movie_title,movie_storyline,poster_image,trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline, 
