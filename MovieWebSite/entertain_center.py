4840db62320add452c340c702ca91dd29a3a69f6
Elliott Foster <elliott.e.foster@gmail.com>
2015-05-17 20:32:58 -0500

    Created modual media wich holds Class Movies, a blue print for movie information. Created test with entertain_center file

---
+++ MovieWebSite/entertain_center.py
@@ -0,0 +1,13 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+						"A story of a boy and his toys that come to life",
+						"http://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+						"www.youtube.com/watch?v=KYz2wyBy3kc")
+						
+avatar = media.Movie("Avatar",
+					 "A marine on an alien planet",
+					 "http://en.wikipedia.org/wiki/Avatar_%282009_film%29#/media/File:Avatar-Teaser-Poster.jpg",
+					 "www.youtube.com/watch?v=cRdxXPV9GNQ")
+toy_story.show_trailer()
+					 
