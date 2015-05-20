99b2f06db7c4b79263f4aa2e68f02ef4df136044
HTM-ell <elliott.e.foster@gmail.com>
2015-05-20 12:07:00 -0500

    Corrected all issues with imdb connecting and obtaining data from database. Need to go back and document my sources
    Created a Class in refine to get the 'global object' idea to work. In
    python don't for get to use self in method defs

--- MovieWebSite/media.py
+++ MovieWebSite/media.py
@@ -2,17 +2,17 @@ import webbrowser
 import refine
 
 class Movie():
-	# about this module	http://imdbpy.sourceforge.net/docs/README.package.txt
-	
-	
-	def __init__(self, movie_title, trailer_youtube):
-		movieID = refine.movieID(movie_title)
-		self.title = refine.title()
-		self.storyline = refine.plot()
-		self.poster_image_url = refine.poster_image()
-		self.trailer_youtube_url =trailer_youtube
-		
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
-		
-	
+        # about this module     http://imdbpy.sourceforge.net/docs/README.package.txt
+        
+        
+        def __init__(self, movie_title, trailer_youtube):
+                movie = refine.IMDbInfo(movie_title)
+                self.title = movie.title()
+                self.storyline = movie.plot()
+                self.poster_image_url = movie.poster_image()
+                self.trailer_youtube_url =trailer_youtube
+                
+        def show_trailer(self):
+                webbrowser.open(self.trailer_youtube_url)
+                
+        
